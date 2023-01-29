b77f8a11a75877b03a667655c4a1765ba098521a
hansikaherath <kavihanhe111@gmail.com>
2023-01-29 16:11:39 +0530

    sidebar navbar setup

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+  const activeMenu = true ;
+
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
             <TooltipComponent content="settings" position="Top">
-              <button>
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%' }}>
                 <FiSettings/>
               </button>
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white"> 
+              sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              sidebar
+            </div>
+          )
+        }
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu ? 'md:ml-72' : 'flex-2'}` 
+         }>
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+            navbar            
+          </div>
+        </div>
         </div>
       </BrowserRouter>
     </div>
