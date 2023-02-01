b592258afaa8aed31cd17c5e89d589fff0a8a1eb
hansikaherath <kavihanhe111@gmail.com>
2023-02-01 22:10:17 +0530

    stat set

--- src/pages/AdminBoard.jsx
+++ src/pages/AdminBoard.jsx
@@ -8,6 +8,7 @@ import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import { earningData, medicalproBranding, recentTransactions, weeklyStats, dropdownData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 import product9 from '../data/product9.jpg';
+import bg1 from '../data/bg1.png';
 
 // const DropDown = ({ currentMode }) => (
 //   <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
@@ -18,7 +19,58 @@ const AdminBoard = () => {
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div>
+        <img
+              className="rounded-sm w-full h-44 lg:w-80 m-3 bg-no-repeat bg-cover bg-center"
+              src={bg1}
+              alt="user-profile"
+            />
+        </div>
+
+          {/* <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        {/* //bg hero pattern is the background image upload backgrong image here */}
+        {/*<div className="flex justify-between items-center">
+              <div>
+                <p className="font-bold text-gray-400">Earnings</p>
+                <p className="text-2xl">$63,448.78</p>
+              </div>
+              {/* <button
+                type="button"
+                style={{ backgroundColor: currentColor }}
+                className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+              >
+                <BsCurrencyDollar />
+              </button> */}
+            {/*</div>
+            <div className="mt-6">
+              <Button
+                color="white"
+                bgColor={'blue'}
+                text="Download"
+                borderRadius="10px"
+                size="md"
+              />
+            </div>
+        </div> */}
+           <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400  mt-1">{item.title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
