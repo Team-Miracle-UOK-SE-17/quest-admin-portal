b751d81d15b2525f971bb09767098227f0d95a08
hansikaherath <kavihanhe111@gmail.com>
2023-02-01 07:21:25 +0530

    complete sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 import Button from './Button';
 const Sidebar = () => {
   
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const { currentColor, activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -18,7 +24,7 @@ const Sidebar = () => {
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (<>
         <div className='flex justify-between items-center'>
-          <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+          <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <SiShopware/> <span>Quest Admin</span>
           </Link>
           <TooltipComponent content="Menu" position="BottomCenter">
@@ -36,7 +42,7 @@ const Sidebar = () => {
             {item.links.map((Link) => (
               <NavLink  to={`/${Link.name}`}
                         key={Link.name}
-                        onClick={()=>{}}
+                        onClick={handleCloseSideBar}
                         className={({ isActive}) => isActive ? activeLink : normalLink }>
                 {Link.icon}
                 <span className="capitalize ">{Link.name}</span>            
