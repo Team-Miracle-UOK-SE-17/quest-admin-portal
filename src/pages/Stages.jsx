a03133b0a38303d43458442f337325312edc144d
hansikaherath <kavihanhe111@gmail.com>
2023-02-03 22:44:44 +0530

    Season Stages

--- src/pages/Stages.jsx
+++ src/pages/Stages.jsx
@@ -6,7 +6,7 @@ import { Header } from '../components';
 
 const Stages = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App" title="Stages" />
+    <Header category="App" title="Stages - Summer Season" />
     <KanbanComponent
       id="kanban"
       keyField="Status"
